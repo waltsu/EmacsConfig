4fb9a107b75e12d12a57589b0eb417ecd7149e49
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2020-03-26 11:36:57 +0200

    No need to specify recipe

--- packages.el
+++ packages.el
@@ -6,7 +6,6 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-(package! add-node-modules-path :recipe (:fetcher github :repo "codesuki/add-node-modules-path"))
+(package! add-node-modules-path)
 (package! prettier-js)
-(package! flow-minor-mode :recipe (:fetcher github :repo "an-sh/flow-minor-mode"))
 (package! company-jedi)
